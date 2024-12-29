0e2ef29bb10cefd02b4d43533a08943c062945e9
Deepkumar Patel <deepthedeveloper97@gmail.com>
2024-12-29 10:40:10 +0530

    Reusable Button Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import Button from "./Button";
+import { GoBell } from "react-icons/go";
+
 function App() {
   return (
     <>
-      Hello There
+      <div><Button primary rounded className="mb-5"> <GoBell/>Hello There!!</Button></div>
+      <div><Button secondary>C</Button></div>
+      <div><Button success>A</Button></div>
+      <div><Button warning outline className="mb-5">D</Button></div>
+      <div><Button danger>A</Button></div>
     </>
   );
 }
